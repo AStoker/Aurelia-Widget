66808b7fe4dc5b7f691841e5ae3f13e3579b0c66
Andrew Stoker <Andy.Stoker@iCloud.com>
2018-08-20 10:38:45 -0400

    added shadycss and moved lib location

--- aurelia_project/tasks/copy-libraries.js
+++ aurelia_project/tasks/copy-libraries.js
@@ -3,10 +3,13 @@ import changedInPlace from 'gulp-changed-in-place';
 
 export default function copyLibraries() {
   const files = [
-      'node_modules/@webcomponents/webcomponentsjs/**/*'
+      'node_modules/@webcomponents/webcomponentsjs/**/*',
+      'node_modules/@webcomponents/shadycss/**/*',
   ];
 
-  return gulp.src(files)
+  return gulp.src(files, {
+      base: 'node_modules/@webcomponents'
+  })
     .pipe(changedInPlace({ firstPass: true }))
-    .pipe(gulp.dest('widget/webcomponentsjs'));
+    .pipe(gulp.dest('widget/libs/'));
 }
