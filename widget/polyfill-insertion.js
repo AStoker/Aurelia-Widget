0e98d9d7f4bf4191f4753e7347192215a3e135c6
Andrew Stoker <Andy.Stoker@iCloud.com>
2018-08-20 09:25:13 -0400

    load initializer after webcomponents loaded

--- widget/polyfill-insertion.js
+++ widget/polyfill-insertion.js
@@ -1,20 +1,25 @@
 
 let polyfillScript = document.createElement('script');
-polyfillScript.src = 'widget/webcomponentsjs/webcomponents-bundle.js';
+polyfillScript.src = 'widget/webcomponentsjs/webcomponents-loader.js';
 polyfillScript.async = false;
 document.head.appendChild(polyfillScript);
 
-// polyfillScript.onload = function() {
-//     WebComponents.waitFor(() => {
-//         // At this point we are guaranteed that all required polyfills have
-//         // loaded, and can use web components API's.
-//         // The standard pattern is to load element definitions that call
-//         // `customElements.define` here.
-//         // Note: returning the import's promise causes the custom elements
-//         // polyfill to wait until all definitions are loaded and then upgrade
-//         // the document in one batch, for better performance.
-//         //return import('my-element.js');
-//         // debugger;
-//     });
+polyfillScript.onload = function() {
+    WebComponents.waitFor(() => {
+        // At this point we are guaranteed that all required polyfills have
+        // loaded, and can use web components API's.
+        // The standard pattern is to load element definitions that call
+        // `customElements.define` here.
+        // Note: returning the import's promise causes the custom elements
+        // polyfill to wait until all definitions are loaded and then upgrade
+        // the document in one batch, for better performance.
+        //return import('my-element.js');
+        // debugger;
+        return new Promise(resolve => {
+            __etrieve.require(['widget/initializer.js'], () => {
+                resolve();
+            });
+        })
+    });
 
-// }
+};
